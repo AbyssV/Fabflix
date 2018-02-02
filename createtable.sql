d174ebcadb3e6ff946beb14bbda7ca49c6532d45
魏景凯慧 <weijingkaihui@dhcp-v074-024.mobile.uci.edu>
2018-02-02 14:51:04 -0800

    everything

---
+++ createtable.sql
@@ -0,0 +1,85 @@
+DROP   SCHEMA   IF   EXISTS   `moviedb`;
+CREATE   SCHEMA   `moviedb`;
+USE   `moviedb`;
+
+CREATE TABLE movies(
+id VARCHAR(10),
+title VARCHAR(100)NOT NULL,
+year INTEGER NOT NULL,
+director VARCHAR(100)NOT NULL,
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE stars(
+id VARCHAR(10),
+name VARCHAR(100)NOT NULL,
+birthYear INTEGER,
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE stars_in_movies(
+starId VARCHAR(10) NOT NULL,
+movieId VARCHAR(10) NOT NULL,
+PRIMARY KEY(starId,movieId),
+-- do i need not null????
+FOREIGN KEY(starId) REFERENCES stars(id) ON DELETE CASCADE,
+FOREIGN KEY(movieId) REFERENCES movies(id) ON DELETE CASCADE
+-- on delete cascade????????
+);
+
+CREATE TABLE genres(
+id INTEGER AUTO_INCREMENT, -- syntax???????
+name VARCHAR(32) NOT NULL,
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE genres_in_movies(
+genreId INTEGER ,
+movieId VARCHAR(10),
+PRIMARY KEY(genreId,movieId),
+FOREIGN KEY(movieId) REFERENCES movies(id) ON DELETE CASCADE,
+FOREIGN KEY(genreId) REFERENCES genres(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE creditcards(
+id VARCHAR(20),
+firstName VARCHAR(50)NOT NULL,
+lastName VARCHAR(50)NOT NULL,
+expiration DATE,
+PRIMARY KEY(id)
+);
+
+
+CREATE TABLE customers(
+id INTEGER AUTO_INCREMENT,
+firstName VARCHAR(50)NOT NULL,
+lastName VARCHAR(50)NOT NULL,
+ccId VARCHAR(20)NOT NULL,
+address VARCHAR(200)NOT NULL,
+email VARCHAR(50)NOT NULL,
+password VARCHAR(20)NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(ccId) REFERENCES creditcards(id) ON DELETE CASCADE
+);
+
+-- relationship
+CREATE TABLE sales(
+id INTEGER AUTO_INCREMENT,
+customerId INTEGER NOT NULL,
+movieId VARCHAR(10)NOT NULL,
+saleDate DATE,
+PRIMARY KEY(id),
+FOREIGN KEY(customerId) REFERENCES customers(id) ON DELETE CASCADE,
+FOREIGN KEY(movieId) REFERENCES movies(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ratings(
+movieId VARCHAR(10),
+rating FLOAT NOT NULL,
+numVotes INTEGER NOT NULL,
+FOREIGN KEY(movieId) REFERENCES movies(id) ON DELETE CASCADE
+);
+
+
+
